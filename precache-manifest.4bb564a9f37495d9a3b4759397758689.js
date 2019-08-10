2fcfff433c7c26a9bde8c6ca9cdc790a2a7c18f0
Richard Hulse <richard@hulse.nz>
2019-08-11 11:32:21 +1200

    Updates

---
+++ precache-manifest.4bb564a9f37495d9a3b4759397758689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f33989378622105235e96d7bc1ec03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9423c7c41a125b7291e2",
+    "url": "/static/css/main.02226152.chunk.css"
+  },
+  {
+    "revision": "ab7f3d6a6b9e573bb301",
+    "url": "/static/js/2.2a79d0a9.chunk.js"
+  },
+  {
+    "revision": "9423c7c41a125b7291e2",
+    "url": "/static/js/main.a1939e53.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
